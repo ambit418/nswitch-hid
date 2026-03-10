64822347bd7b2d75aab81ee9e6eae030d8ce32b5
MacMini <zhizhidac@gmail.com>
2026-03-10 21:11:10 +0900

    feat: initial no_std library setup

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+//! Nintendo Switch Pro Controller (HORI Pokken Controller) USB HID emulation.
+//!
+//! A `no_std` library for creating USB HID reports compatible with
+//! the Nintendo Switch.
+
+#![no_std]
+#![warn(missing_docs)]
